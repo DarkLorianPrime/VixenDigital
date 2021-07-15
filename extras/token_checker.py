6deb0e698c9ee4e6cc060d014559ffd6eb55c59d
DarkLorian <lolpokens@mail.ru>
2021-07-15 05:12:04 +0400

    Update_in_dev

---
+++ extras/token_checker.py
@@ -0,0 +1,12 @@
+from rest_framework.response import Response
+
+from catalogs.models import Access_Token
+
+
+def token_checker(fn):
+    def wrapper(*args, **kwargs):
+        if not Access_Token.objects.filter(token=args[1].GET['access_token']).exists():
+            return Response({'response': {'code': '3', 'error': 'token not found'}}, status=400)
+        return fn(*args, **kwargs)
+
+    return wrapper
