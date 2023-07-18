cb46ca10eff082756154f2781ed192529a87547a
darklorian <lolpokens@mail.ru>
2023-07-19 03:21:25 +0400

    Fix tests environment #8

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-python manage.py migrate
-python manage.py init_elastic
-python manage.py initialize_buckets
 
 echo "FTG_TEST: $TEST"
 
@@ -12,6 +9,10 @@ until curl http://elk:9200; do
   echo "not working..."
   sleep 1
 done
+
+python manage.py migrate
+python manage.py init_elastic
+python manage.py initialize_buckets
 
 if [ "$TEST" == "true" ]; then
   python manage.py test
