5761658bc90299858204695a0ea6059375cec2c2
darklorian <lolpokens@mail.ru>
2023-07-19 02:08:58 +0400

    Fix tests environment

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,7 +3,7 @@ python manage.py migrate
 python manage.py init_elastic
 python manage.py initialize_buckets
 if [ "$TEST" == "true" ]; then
-python manage.py test
+  python manage.py test
 else
-python manage.py runserver 0.0.0.0:8000
+  python manage.py runserver 0.0.0.0:8000
 fi
