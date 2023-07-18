b2489d11c90a09adc4f35bbae192462983843050
darklorian <lolpokens@mail.ru>
2023-07-19 03:09:28 +0400

    Fix tests environment #6

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
 python manage.py migrate
 python manage.py init_elastic
 python manage.py initialize_buckets
+
 echo "FTG_TEST: $TEST"
 
+apt-get -y update;
+apt-get -y install curl
 until curl http://elk:9200; do
   echo "not working..."
   sleep 1
