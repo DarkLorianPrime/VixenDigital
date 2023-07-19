ebe5cd8c724eb249fb3b49816043cfe243b9fdfa
darklorian <lolpokens@mail.ru>
2023-07-19 13:32:46 +0400

    Fix tests environment #9.6.2F

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,7 +2,7 @@
 set -ex
 exec "$@"
 
-echo "FTG_TEST: $1, $0"
+echo "FTG_TEST: $1, $0, $TEST"
 
 apt-get -y update;E
 apt-get -y install curl
@@ -15,7 +15,7 @@ python manage.py migrate
 python manage.py init_elastic
 python manage.py initialize_buckets
 echo
-if [ "$TEST" = "true" ]; then
+if [ "$TEST" = "yes" ]; then
   python manage.py test
 else
   python manage.py runserver 0.0.0.0:8000
