25dc49387b1e90f7967940027c164c0b57559d18
darklorian <lolpokens@mail.ru>
2023-07-25 02:10:53 +0400

    Add NGINX and remove django actions test

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,17 +2,10 @@
 
 apt-get -y update;
 apt-get -y install curl
-until curl http://elk:9200; do
-  echo "not working..."
-  sleep 1
-done
 
 python manage.py migrate
 python manage.py init_elastic
 python manage.py initialize_buckets
 
-if [ "$TEST" = "yes" ]; then
-  python manage.py test
-else
-  python manage.py runserver 0.0.0.0:8000
-fi
+python manage.py runserver 0.0.0.0:8000
+
